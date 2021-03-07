b54678a90facc4700b3925a20876a8bd22b89103
Zaw Myo Tun <zawmyotun11@gmail.com>
2021-03-08 07:51:59 +0800

    Add describe_data()

--- R/describe_data.R
+++ R/describe_data.R
@@ -1,3 +1,24 @@
+#' Create a tableby object using {arsenal} package with more intuitive default parameters. The tableby object should be passed to summary() to create a descriptive table.
+#'
+#' @param dat A dataframe
+#' @param by A string with length 1. Name of grouping variable by which data are described. If "" (default), overall total will be shown.
+#' @param p_value A logical vector with length 1. Toggle to show p value (default is FALSE).
+#' @param total A logical vector with length 1. Toggle to show overall total (default is FALSE) when grouping variable is defined.
+#' @param ... Arguments passed to tableby(). ?tableby for more details.
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' library(dplyr)
+#' data(mockstudy)
+#'
+#' mockstudy %>%
+#'   select(arm, sex, age) %>%
+#'   describe_data(by = "arm") %>%
+#'   summary(text = TRUE)
+#' }
 describe_data <- function(dat, by = "", p_value = FALSE, total = FALSE, ...) {
   vars <- names(dat)
   vars <- if (by == "") {
@@ -9,10 +30,10 @@ describe_data <- function(dat, by = "", p_value = FALSE, total = FALSE, ...) {
   f <- paste(by, vars, sep = " ~ ")
 
   tab <- arsenal::tableby(
-    as.formula(f),
+    stats::as.formula(f),
     data = dat,
     test = p_value,
-    total = total
+    total = total,
+    ...
   )
-  summary(tab, ...)
 }

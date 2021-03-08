140fb58fe94e55b97c7e7cdaeeebb8e2922ccf44
Zaw Myo Tun <zawmyotun11@gmail.com>
2021-03-08 08:14:28 +0800

    Update describe_data()

--- R/describe_data.R
+++ R/describe_data.R
@@ -1,9 +1,14 @@
-#' Create a tableby object using {arsenal} package with more intuitive default parameters. The tableby object should be passed to summary() to create a descriptive table.
+#' Create a tableby object using {arsenal} package with more intuitive default parameters.
+#'
+#' The tableby object should be passed to summary() to create a descriptive table.
 #'
 #' @param dat A dataframe
-#' @param by A string with length 1. Name of grouping variable by which data are described. If "" (default), overall total will be shown.
-#' @param p_value A logical vector with length 1. Toggle to show p value (default is FALSE).
-#' @param total A logical vector with length 1. Toggle to show overall total (default is FALSE) when grouping variable is defined.
+#' @param by A string. Name of grouping variable by which data are described. If "" (default), overall total will be shown.
+#' @param p_value A logical. Toggle to show p value (default is FALSE).
+#' @param total A logical. Toggle to show overall total (default is FALSE) when grouping variable is defined.
+#' @param digits An integer. Control the number of decimal place for continuous values (default is 1).
+#' @param digits.pct An integer. Control the number of decimal place for percents (default is 1).
+#' @param digits.p An integer. Control the number of decimal place for p-values (default is 1).
 #' @param ... Arguments passed to tableby(). ?tableby for more details.
 #'
 #' @return
@@ -11,15 +16,29 @@
 #'
 #' @examples
 #' \dontrun{
+#' library(arsenal)
 #' library(dplyr)
+#'
 #' data(mockstudy)
 #'
 #' mockstudy %>%
 #'   select(arm, sex, age) %>%
 #'   describe_data(by = "arm") %>%
 #'   summary(text = TRUE)
+#'
+#' mockstudy %>%
+#'   select(arm, sex, age) %>%
+#'   describe_data() %>%
+#'   summary()
 #' }
-describe_data <- function(dat, by = "", p_value = FALSE, total = FALSE, ...) {
+describe_data <- function(dat,
+                          by = "",
+                          p_value = FALSE,
+                          total = FALSE,
+                          digits = 1L,
+                          digits.pct = 1L,
+                          digits.p = 4L,
+                          ...) {
   vars <- names(dat)
   vars <- if (by == "") {
     paste(vars, collapse = " + ")

0c26bbe38fbf8047332167adbddd047fb8fa952d
Zaw Myo Tun <zawmyotun11@gmail.com>
2021-03-22 16:50:34 +0800

    Add prefix: data_set_labels

--- R/data_set_labels.R
+++ R/data_set_labels.R
@@ -1,8 +1,8 @@
-#' Add `label` attribute to columns in a dataframe
+#' Set labels to columns in a dataframe.
 #'
-#' @param .data A dataframe.
-#' @param .cols A character vector. Colomns to add labels.
-#' @param .labels A character vector. Labels for the columns. If NULL, each element of .colnames must be named with its label.
+#' @param .data A dataframe
+#' @param .cols A character vector; Colomns to set labels to
+#' @param .labels A character vector; Labels for the columns. If NULL, each element of .colnames must be named with its label.
 #' @return Dataframe with labelled columns.
 #' @examples
 #' \dontrun{
@@ -14,14 +14,14 @@
 #' # Labels in a separate vector
 #' var <- c("age", "gender")
 #' lbl <- c("Age of participant", "Gender of participant")
-#' set_labels(dat, var, lbl)
+#' data_set_labels(dat, var, lbl)
 #'
 #' # Labels in the names of `.cols` vector
 #' names(var) <- lbl
-#' set_labels(dat, var)
+#' data_set_labels(dat, var)
 #' }
 #' @export
-set_labels <- function(.data, .cols, .labels = NULL) {
+data_set_labels <- function(.data, .cols, .labels = NULL) {
   stopifnot(
     "`.data` must be data.frame." = is.data.frame(.data),
     "`.cols` must be character." = is.vector(.cols, mode = "character"),
--- tests/testthat/test-data_set_labels.R
+++ tests/testthat/test-data_set_labels.R
@@ -1,6 +1,6 @@
 test_that("Adding labels to columns in a dataframe works", {
   dat <- data.frame(a = 1:3)
-  dat <- set_labels(dat, "a", "Example")
+  dat <- data_set_labels(dat, "a", "Example")
 
   expect_equal(attributes(dat$a)$label, "Example")
 })
